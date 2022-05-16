e2098ac4aabbeec871f23f3d4e6170763b43c0a4
bbammju <stwinsilver2@gmail.com>
2022-05-16 23:13:13 +0900

    second

--- src/buttons/mypage_goodsentencebtn.js
+++ src/buttons/mypage_goodsentencebtn.js
@@ -3,23 +3,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Mypage_goodsentencebtn = styled.button`
+position: absolute;
+width: 38%;
+height: 10%;
+top: 45%;
+left: 31%;
+border-radius: 1rem;
+font-size: 2rem;
+text-align: center;
+`
 
-const Mypage_goodsentencebtn = () => {
 
-  const Mypage_goodsentencebtn = styled.button`
-  position: absolute;
-  width: 38%;
-  height: 10%;
-  top: 45%;
-  left: 31%;
-  border-radius: 1rem;
-  font-size: 2rem;
-  text-align: center;
-  `
+const MypageGoodsentencebtn = () => {
+
   return (
     <section className="main">
       {/* TODO : About 메뉴 아이콘과 Mypage 메뉴 아이콘을 작성하고 Link 컴포넌트를 이용하여 경로(path)를 연결합니다. */}
-
       <Link to="/mypage_goodsentence">
       <Mypage_goodsentencebtn>오늘의 명언</Mypage_goodsentencebtn>
       </Link>
@@ -28,4 +28,4 @@ const Mypage_goodsentencebtn = () => {
   );
 };
 
-export default Mypage_goodsentencebtn;
+export default MypageGoodsentencebtn;
